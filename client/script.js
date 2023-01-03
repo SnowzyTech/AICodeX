4a62deee525662f96c415435cc2c28435c9991e8
ihekuna Emmanuel <ihekunaemmanuelc@gmail.com>
2023-01-03 01:39:51 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server - > bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-ai-um5p.onrender.com/', {
     method: "POST",
     headers: {
       'Content-Type': 'application/json'
