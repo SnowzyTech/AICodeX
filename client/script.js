dc4722fe2722a7b24fd485c21070b139c61152b6
ihekuna Emmanuel <ihekunaemmanuelc@gmail.com>
2023-01-03 11:56:34 +0100

    updated server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server - > bot's response
 
-  const response = await fetch('https://aicode-ynim.onrender.com/', {
+  const response = await fetch(`https://aicode-ynim.onrender.com/`, {
     method: "POST",
     headers: {
       'Content-Type': 'application/json'
