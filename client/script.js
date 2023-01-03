54fb303395a41df945966adaafe924e79dfaeb56
ihekuna Emmanuel <ihekunaemmanuelc@gmail.com>
2023-01-03 10:55:56 +0100

    updated the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server - > bot's response
 
-  const response = await fetch('https://codexchat-16q5.onrender.com/', {
+  const response = await fetch('http://localhost:5000', {
     method: "POST",
     headers: {
       'Content-Type': 'application/json'
